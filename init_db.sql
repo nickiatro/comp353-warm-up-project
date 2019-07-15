e8c57d28a328ac90379a2981e0f9913fa2747790
Anne-Laure <alehresmann@gmail.com>
2019-07-15 12:37:24 -0400

    add more tables for term and courses, add id

--- init_db.sql
+++ init_db.sql
@@ -5,35 +5,66 @@ DROP DATABASE IF EXISTS proj;
 CREATE DATABASE proj;
 USE proj;
 
-CREATE TABLE Departments(
-    ID INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name CHAR(100));
 
+
+
+CREATE TABLE Departments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name CHAR(100) );
+
+-- needs a trigger to insert default credit count if credit req is null, based on degree enum
 CREATE TABLE Programs(
-    ID INT UNSIGNED PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name CHAR(100),
     degree ENUM("undergraduate", "graduate") NOT NULL,
-    credit_req INT UNSIGNED);
+    department_id INT REFERENCES Departments(id),
+    credit_req INT UNSIGNED
+);
 
--- This is necessary to fulfill the "one or more" requirement.
--- If you have a better idea, feel free to change this.
-CREATE TABLE Department_Programs (
-    Department_ID INT UNSIGNED REFERENCES Departments(ID),
-    Program_ID INT UNSIGNED REFERENCES Programs(ID));
+CREATE TABLE Term(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    season ENUM("Fall", "Winter", "Summer"),
+    year YEAR(4)
+);
+
+CREATE TABLE AdvisorTermProgram(
+    instructor_id INT REFERENCES Instructors(id),
+    term_id INT REFERENCES Term(id),
+    year YEAR(4)
+);
+
+
+CREATE TABLE course (
+    name CHAR(100),
+    department_name CHAR(100),
+    courseNumber INT,
+    section CHAR(4)
+);
+
+--CREATE TABLE Prereqs (
+--
+--);
 
 CREATE TABLE Students(
     id INT PRIMARY KEY,
     first_name CHAR(100) NOT NULL,
     last_name CHAR(100) NOT NULL,
-    department CHAR(100),
-    degree ENUM("undergraduate", "graduate") NOT NULL,
-    FOREIGN KEY (department) REFERENCES Departments(name)
+    gpa DECIMAL(3, 2) UNSIGNED,
+    department_id INT REFERENCES Departments(id),
+    degree ENUM("undergraduate", "graduate") NOT NULL
 );
 
+-- needs a trigger to verify existence of pair (program_name, department_name)
+CREATE TABLE StudentsPrograms(
+    student_id INT REFERENCES Students(id),
+    program_id INT REFERENCES Programs(id),
+    department_id INT REFERENCES Departments(id),
+    PRIMARY KEY(student_id, program_id, department_id)
+);
+--
 CREATE TABLE Instructors(
     id INT PRIMARY KEY,
     first_name CHAR(100) NOT NULL,
     last_name CHAR(100) NOT NULL,
-    department CHAR(100),
-    FOREIGN KEY (department) REFERENCES Departments(name)
+    department_id INT REFERENCES Departments(id)
 );
