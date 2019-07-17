4c7f2573edd0d84b93f206fd07ec430979a9435b
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-07-17 10:34:40 -0400

    updated home.php styling

--- home.php
+++ home.php
@@ -10,3 +10,34 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<style>
+    body, html{
+        height: 100%;
+    }
+    .background {
+        background-image: url("welcome_background.jpg");
+        filter: blur(8px);
+        -webkit-filter: blur(8px);
+        height: 100%;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+</style>
+<title>Welcome to the Concordia University Database System</title>
+<body>
+<div class="background"></div>
+<div class="titleBox">
+
+</div>
+
+</body>
+</html>
+
+
