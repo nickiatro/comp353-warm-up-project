5c9cec13153a59d006caa02b21aa1d07a4d55fe0
Nicholas Iatropoulos <nicholasiatropoulos@hotmail.com>
2019-07-18 23:12:43 -0400

    added sql query and error message gets displayed when query fails

--- search.php
+++ search.php
@@ -1,10 +1,15 @@
 <?php
+session_start();
+
+if(!isset($_SESSION)) {
+    $_SESSION["attributes"] = $_SESSION["tables"] = $_SESSION["search"] = "";
+}
 
 $server = "localhost:3306";
 $username = "prc353_1";
 $password = "h3g1m8ca";
 
-$conn = new mysqli($server, $username, $password);
+$conn = new mysqli($server, $username, $password, "prc353_1");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -32,15 +37,20 @@ $sqlFile = fopen("init_db.sql", "r");
 
 $attributes = [];
 $fileContents = explode("\n", fread($sqlFile, filesize("init_db.sql")));
-print_r($fileContents);
+fclose($sqlFile);
 
-$tableSelect = "style = \"visibility: hidden;\"";
+$tableSelect = "";
 $attributeSelect = "style = \"visibility: hidden;\"";
-$enterCriteria = "style = \"visibility: hidden;\"";
+$sqlError = "style = \"visibility: hidden;\"";
+$showResults = "style = \"visibility: hidden;\"";
 
 $tablesInQuery = "";
-if ($tablesInQuery == "") {
-    if (isset($_POST["submit"])) {
+$attributesInQuery = "";
+$searchCriteria = "";
+
+$sqlErrorMessage="";
+
+    if (isset($_POST["submit1"])) {
         if (isset($_POST["tables"])) {
             for ($i = 0; $i < count($_POST["tables"]); $i++) {
                 $tablesInQuery = $tablesInQuery . $_POST["tables"][$i];
@@ -48,32 +58,70 @@ if ($tablesInQuery == "") {
                     $tablesInQuery = $tablesInQuery . ", ";
                 }
             }
+
         }
+        $_SESSION["tables"] = $tablesInQuery;
+        $tableSelect = "style = \"visibility: hidden;\"";
+        $attributeSelect = "style = \"visibility: visible;\"";
     }
-    $tableSelect = "style = \"visibility: visible;\"";
-}
 
-if ($tablesInQuery != "") {
-    $tableSelect = "style = \"visibility: hidden;\"";
-    $attributeSelect = "style = \"visibility: visible;\"";
-}
-
-$attributesInQuery = "";
-
-if ($attributesInQuery == "" && $tablesInQuery != "") {
-    if (isset($_POST["submit"])) {
+    elseif (isset($_POST["submit2"])) {
         if (isset($_POST["attributes"])) {
             for ($i = 0; $i < count($_POST["attributes"]); $i++) {
                 $attributesInQuery = $attributesInQuery . $_POST["attributes"][$i];
                 if ($i + 1 != count($_POST["attributes"])) {
                     $attributesInQuery = $attributesInQuery . ", ";
                 }
-            }
+                $_SESSION["attributes"] = $attributesInQuery;
+                $tableSelect = "style = \"visibility: hidden;\"";
+                $attributeSelect = "style = \"visibility: hidden;\"";}
+        }
+        if (isset($_POST["search"])){
+            $_SESSION["search"] = $_POST["search"];
+        }
+        $query = "SELECT " . $_SESSION["attributes"] . " FROM " . $_SESSION["tables"] . " " . $_SESSION["search"] . ";";
+
+        if ($conn->query($query)) {
+
+        }
+
+        if (!$conn->query($query)) {
+            $sqlErrorMessage = $conn->error;
+        }
+
+        if ($sqlErrorMessage != ""){
+            $sqlError = "style = \"visibility: visible;\"";
+            $tableSelect = "style = \"visibility: hidden;\"";
         }
     }
-    $attributeSelect = "style = \"visibility: visible;\"";
+
+$selectedTables = explode(",", $tablesInQuery);
+$result = [];
+
+for ($i = 0; $i < count($selectedTables); $i++) {
+    $query = $conn->query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'prc353_1' AND TABLE_NAME = '$selectedTables[$i]'");
+
+    while ($row = $query->fetch_assoc()) {
+        $result[] = $row;
+    }
+
+    $columnArr = array_column($result, 'COLUMN_NAME');
+
+    for ($j = 0; $j < count($columnArr); $j++) {
+        $columnArr[$j] = $selectedTables[$i] . "." . $columnArr[$j];
+    }
+
+    $attributes = array_merge($attributes, $columnArr);
 }
 
+$countArray = [];
+
+for ($i = 0; $i < count($attributes); $i++) {
+    $countString = "COUNT(".$attributes[$i].")";
+    array_push($countArray, $countString);
+}
+
+$attributes = array_merge($attributes, $countArray);
 ?>
 
 <!doctype html>
@@ -131,6 +179,24 @@ if ($attributesInQuery == "" && $tablesInQuery != "") {
         text-align: center;
     }
 
+    .search {
+        background-color: rgb(128, 128, 128);
+        background-color: rgba(128,128,128, 0.4);
+        color: lightgrey;
+        font-weight: bold;
+        font-size: 100%;
+        font-family: "Cambria Math", sans-serif;
+        border: 3px solid #f1f1f1;
+        position: absolute;
+        top: 25%;
+        left: 50%;
+        transform: translate(-50%, 140%);
+        z-index: 2;
+        width: 40%;
+        padding: 20px;
+        text-align: center;
+    }
+
     a{
         color: lightgrey;
     }
@@ -165,7 +231,7 @@ if ($attributesInQuery == "" && $tablesInQuery != "") {
             }
             echo "</select><br/>";
             ?>
-        <input type="submit" name="submit">
+        <input type="submit" name="submit1">
     </form>
 </div>
 <div class="selection" <?php echo $attributeSelect;?>>
@@ -180,16 +246,31 @@ if ($attributesInQuery == "" && $tablesInQuery != "") {
         }
         echo "</select><br/>";
         ?>
-        <input type="submit" name="submit">
+        <div class="search" <?php echo $attributeSelect;?>>
+            <label>Please Enter the Search Criteria <br/>
+                <small><em>Please Use MySQL Syntax</em></small>
+            </label>
+            <input type="text" name="search"/>
+            <input type="submit" name="submit2">
+        </div>
     </form>
 </div>
-<div class="selection" <?php echo $enterCriteria;?>>
-    <form name="criteriaForm" action="./search.php" method="post">
-        <label>Please Enter the Search Criteria <br/>
-            <small><em>Please Use MySQL Syntax</em></small>
-        </label>
-        <input type="text" name="search"/>
-        <input type="submit" name="submit">
+<div class="selection" <?php echo $sqlError;?>>
+    <form name="tablesForm" action="./search.php" method="post">
+        <label>MySQL Error Message</label>
+        <?php
+            echo "<p>$sqlErrorMessage</p>";
+        ?>
+        <a href="./home.php">Return to Homepage</a>
+    </form>
+</div>
+<div class="selection" <?php echo $showResults;?>>
+    <form name="tablesForm" action="./search.php" method="post">
+        <label>Results<br /></label>
+        <?php
+            
+        ?>
+        <a href="./home.php">Return to Homepage</a>
     </form>
 </div>
 </body>
